eca64c7fcd2c8b74a11772391e4877575892a958
Andy Wermke <andy.dev@next-step-software.com>
2019-10-19 18:05:04 +0200

    Improve `Router()` type

--- test/middleware.test.ts
+++ test/middleware.test.ts
@@ -1,6 +1,6 @@
 import test from "ava"
 import request from "supertest"
-import { composeMiddlewares, Middleware, Request, Response, Route, Router, Service } from "../src/index"
+import { Middleware, Request, Response, Route, Router, Service } from "../src/index"
 
 test("can assign new props to a request", async t => {
   interface CustomRequest extends Request {
@@ -17,15 +17,19 @@ test("can assign new props to a request", async t => {
   const router = Router([
     fooMiddleware,
 
-    Route.GET("/test", (request: CustomRequest) => {
+    Route.GET("/", (request: CustomRequest) => {
       return Response.Text(200, request.foo)
+    }),
+
+    Route.GET("/x", (request: Request) => {
+      return Response.Text(200, "A route handler consuming a default request object should not break the type check. " + request.url)
     })
   ])
-  const service = Service(router)
 
+  const service = Service(router)
   const response = await request(service.handler())
     .get("/")
     .expect(200)
 
-  t.is(response.text, "foo")
+  t.is(response.text, "bar")
 })
